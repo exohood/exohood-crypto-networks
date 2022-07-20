1ba4ad8b2bc8ca58e07687fc71411a3b8b490b89
Shira Otsuki <107924319+ShiraOtsuki@users.noreply.github.com>
2022-07-20 06:06:34 -0400

    Create WrapperType.java

---
+++ blockchain-web3/src/main/java/io/exohood/web3/blockchain/model/WrapperType.java
@@ -0,0 +1,18 @@
+package io.exohood.web3.blockchain.model;
+
+import lombok.Getter;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Type;
+
+@Getter
+public class WrapperType<T extends Type> {
+    private TypeReference<T> typeReference;
+
+    private WrapperType(TypeReference<T> typeReference) {
+        this.typeReference = typeReference;
+    }
+
+    public static <T extends Type> WrapperType<T> create(final Class<T> c, boolean indexed) {
+        return new WrapperType<>(TypeReference.create(c, indexed));
+    }
+}
